7924d1ce77be8652fb9bd8ab6571d2cc71c78256
Ryohta <ryohta@RyohtanoMacBook-Pro.local>
2020-05-06 16:11:11 +0900

    20200506-comnet-end

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
-  root 'posts#index'
+    root 'posts#index'
 
-  # usersコントローラーのshowアクションのルーティングを追加
-   get '/users/:id', to: 'users#show', as: 'user'
+    # usersコントローラーのshowアクションのルーティングを追加
+    get '/users/:id', to: 'users#show', as: 'user'
 
-  #get 'users/', to: 'users#show', as: 'user_show'
-  # get 'users/', to: 'users#show', as: 'user'
-  # resources :users, only: %i(show show)
+    #get 'users/', to: 'users#show', as: 'user_show'
+    # get 'users/', to: 'users#show', as: 'user'
+    # resources :users, only: %i(show show)
 
   # 投稿機能で使うルーティングの追加
-  resources :posts, only: %i(index new create show destroy) do
+    resources :posts, only: %i(index new create show destroy) do
     resources :photos, only: %i(create)
 
 
-  resources :likes, only: %i(create destroy)
+    resources :likes, only: %i(create destroy)
+    resources :comments, only: %i(create destroy)
   end
-  end
+end
